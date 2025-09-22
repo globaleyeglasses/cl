fa6af27b9dd70db0bf9ba7843997de105a2d4402
Your Name <you@example.com>
2025-09-22 17:14:07 +0530

    f

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cocoleni_optics",
-    version="0.2.0",
+    version="0.2.1",
     description="COCO LENI Optics App with Prescription, Lens Order, Face Scan 3D, and Sales Order Links",
     author="COCO LENI",
     packages=find_packages(),
